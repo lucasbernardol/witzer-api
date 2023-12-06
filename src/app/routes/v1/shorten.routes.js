01fa9e0ad9a7cb7c7263e70b44d3ac2880dfcea1
lucasbernardoph <lucasbernardo.touch@gmail.com>
2023-12-06 19:04:26 -0300

    added rate limiter

--- src/app/routes/v1/shorten.routes.js
+++ src/app/routes/v1/shorten.routes.js
@@ -1,15 +1,50 @@
 import { Router } from 'express';
 import { celebrate, Segments } from 'celebrate';
 
+import { rateLimit } from 'express-rate-limit';
+import { RedisStore } from 'rate-limit-redis';
+
+import HttpError from 'http-errors';
+
 import ShortenController from '../../controllers/ShortenController.js';
 import { RawOrJson } from '../../validations/types/format.js';
 
 import { hashSchema } from '../../validations/hashSchema.js';
 import { urlSchema } from '../../validations/urlSchema.js';
 
+import { redisClient } from '../../../modules/redis/client.js';
+
 const controller = new ShortenController();
 
 const routes = Router();
+
+const redisStore = new RedisStore({
+  sendCommand: (...args) => redisClient.sendCommand(args),
+});
+
+const rateLimiterHandler = (request, response, next, { message }) => {
+  return next(new HttpError.TooManyRequests(message));
+};
+
+const shortCreateLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 5,
+  standardHeaders: true,
+  legacyHeaders: false,
+  keyGenerator: (request) => `L-CREATE:${request.ip}`,
+  handler: rateLimiterHandler,
+  store: redisStore,
+});
+
+const shortDeleteLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 1,
+  standardHeaders: true,
+  legacyHeaders: false,
+  keyGenerator: (request) => `L-DELETE:${request.ip}`,
+  handler: rateLimiterHandler,
+  store: redisStore,
+});
 
 routes.get(
   '/shorts/:hash/views',
@@ -30,12 +65,14 @@ routes.get(
 
 routes.post(
   '/shorts',
+  shortCreateLimiter,
   celebrate({ [Segments.BODY]: urlSchema() }),
   controller.create,
 );
 
 routes.delete(
   '/shorts/:hash',
+  shortDeleteLimiter,
   celebrate({ [Segments.PARAMS]: hashSchema() }),
   controller.delete,
 );
--- src/modules/redis/client.js
+++ src/modules/redis/client.js
@@ -4,3 +4,7 @@ import redis from 'redis';
 export const redisClient = redis.createClient({
   url: process.env.REDIS_URI,
 });
+
+(async () => {
+  await redisClient.connect().then(() => console.log('\nREDIS: OK'));
+})();
